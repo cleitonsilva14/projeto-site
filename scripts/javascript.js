76c1c35a8fbe0ebcb998d5a38fa9748d1123285d
Cleiton <111751059+cleitonsilva14@users.noreply.github.com>
2022-08-31 13:30:29 -0300

    Obter valores dos ranges inputs

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,3 +1,7 @@
 window.addEventListener("load", ()=>{
     console.log("Hello")
+    const red = document.getElementById('r').value
+    const green = document.getElementById('g').value
+    const blue = document.getElementById('b').value
+    console.log(red, green, blue)
 })
