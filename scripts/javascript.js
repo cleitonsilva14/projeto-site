7076bd35ac2d105a05578862cc44b524587e01ad
Cleiton <111751059+cleitonsilva14@users.noreply.github.com>
2022-08-31 14:07:47 -0300

    Change colors
    Adicionado mousemove input range

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,7 +1,37 @@
-window.addEventListener("load", ()=>{
-    console.log("Hello")
-    const red = document.getElementById('r').value
-    const green = document.getElementById('g').value
-    const blue = document.getElementById('b').value
-    console.log(red, green, blue)
+const red = document.querySelector('input#r')
+const green = document.querySelector('input#g')
+const blue = document.querySelector('input#b')
+
+
+let valueRed = red.value
+let valueGreen = green.value
+let valueBlue = blue.value
+
+const divColor = document.querySelector('div.container')
+
+
+function updateColor(){
+    divColor.style.backgroundColor = `rgb(${valueRed}, ${valueGreen}, ${valueBlue})`
+}
+
+
+red.addEventListener('mousemove', ()=>{
+    valueRed = red.value
+    document.querySelector("span#spanRed").innerHTML = document.querySelector('input#r').value
+    updateColor()
+
 })
+
+green.addEventListener('mousemove', ()=>{
+    valueGreen = green.value
+    document.querySelector("span#spanGreen").innerHTML = document.querySelector('input#g').value
+    updateColor()
+})
+
+blue.addEventListener('mousemove', ()=>{
+    valueBlue = blue.value
+    document.querySelector("span#spanBlue").innerHTML = document.querySelector('input#b').value
+    updateColor()
+})
+
+console.log(valueRed, valueGreen, valueBlue)
