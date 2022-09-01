d96a8d8ec0f4553b0b20988cf4b41ed1a3ed2faa
Cleiton <111751059+cleitonsilva14@users.noreply.github.com>
2022-09-01 20:34:34 -0300

    Update javascript.js

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,37 +1,4 @@
-const red = document.querySelector('input#r')
-const green = document.querySelector('input#g')
-const blue = document.querySelector('input#b')
-
-
-let valueRed = red.value
-let valueGreen = green.value
-let valueBlue = blue.value
-
-const divColor = document.querySelector('div.container')
-
-
-function updateColor(){
-    divColor.style.backgroundColor = `rgb(${valueRed}, ${valueGreen}, ${valueBlue})`
+window.onload = function(){
+    const inputRed = document.querySelector('#r')
+    
 }
-
-
-red.addEventListener('mousemove', ()=>{
-    valueRed = red.value
-    document.querySelector("span#spanRed").innerHTML = document.querySelector('input#r').value
-    updateColor()
-
-})
-
-green.addEventListener('mousemove', ()=>{
-    valueGreen = green.value
-    document.querySelector("span#spanGreen").innerHTML = document.querySelector('input#g').value
-    updateColor()
-})
-
-blue.addEventListener('mousemove', ()=>{
-    valueBlue = blue.value
-    document.querySelector("span#spanBlue").innerHTML = document.querySelector('input#b').value
-    updateColor()
-})
-
-console.log(valueRed, valueGreen, valueBlue)
