d82bb20e943e2ffbb101d7ec80dd5dff27ac294c
Cleiton <111751059+cleitonsilva14@users.noreply.github.com>
2022-09-01 20:55:53 -0300

    Funcionalidade Javascript
    Adicionadas funções para o input range com javascript

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,42 @@
 window.onload = function(){
-    const inputRed = document.querySelector('#r')
+    const inputRed = document.querySelector('input#red')
+    const inputGreen = document.querySelector('input#green')
+    const inputBlue = document.querySelector('input#blue')
+
+    const spanRed = document.querySelector('span#spanRed')
+    const spanGreen = document.querySelector('span#spanGreen')
+    const spanBlue = document.querySelector('span#spanBlue')
+
+    const container = document.querySelector('div#container')
+
+    inputRed.addEventListener('input', function(){
+        //liveUpdate(this.value, this.name)
+        liveUpdate()
+    })
+
+    inputGreen.addEventListener('input', function(){
+        liveUpdate()
+    })
+
+    inputBlue.addEventListener('input', function(){
+        liveUpdate()
+    })
+
+    function liveUpdate(){
+        let r = inputRed.value
+        let g = inputGreen.value
+        let b = inputBlue.value
+        
+        spanRed.innerText = r
+        spanGreen.innerText = g
+        spanBlue.innerText = b
+        
+        container.style.backgroundColor = `rgb(${r},${g},${b})`
+
+    }
+
+
+
+    //alert(`${inputRed.value}, ${inputGreen.value}, ${inputBlue.value}`)
     
 }
